f80ca5d6a026dcd1f3d2b0ef0e0c326d0a043689
ScrewThisNoise <51236954+ScrewThisNoise@users.noreply.github.com>
2019-11-10 00:46:20 +0100

    Made AssemblyInfo common

--- src/SharedCode/AssemblyInfo.cs
+++ src/SharedCode/AssemblyInfo.cs
@@ -1,1 +1,23 @@
-﻿
+﻿using System.Resources;
+using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Windows;
+
+[assembly: AssemblyVersion("1.4.1.0")]
+[assembly: CompilationRelaxations(8)]
+[assembly: RuntimeCompatibility(WrapNonExceptionThrows = true)]
+[assembly: Debuggable(DebuggableAttribute.DebuggingModes.IgnoreSymbolStoreSequencePoints)]
+[assembly: AssemblyTitle("IllusionLaunchers")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("ScrewThisNoise")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("ScrewThisNoise")]
+[assembly: AssemblyTrademark("")]
+[assembly: ComVisible(false)]
+[assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
+[assembly: AssemblyFileVersion("1.4.1.0")]
+[assembly: NeutralResourcesLanguage("en-GB")]
