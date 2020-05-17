3d507e753d764c8819c7781785541b861e912c7e
ScrewThisNoise <51236954+ScrewThisNoise@users.noreply.github.com>
2020-05-17 23:35:40 +0200

    Regenerated Localizable.Designer.cs

--- src/SharedCode/ToggleAdder.cs
+++ src/SharedCode/ToggleAdder.cs
@@ -31,10 +31,10 @@ namespace InitSetting
             Modlist.Add(new Toggleables("AI_Graphics", Localizable.ToggleAiGraphics, "AI_Graphics", null, false));
             Modlist.Add(new Toggleables("DHH", Localizable.ToggleDhh, "DHH_AI4", null, false));
             Modlist.Add(new Toggleables("DHHPH", Localizable.ToggleDhh, "ProjectHighHeel", null, true));
-            Modlist.Add(new Toggleables("HoneyPot", "HoneyPot", "HoneyPot", null, true));
-            Modlist.Add(new Toggleables("RimRemover", "RimRemover", "RimRemover", null, false));
-            Modlist.Add(new Toggleables("Stiletto", "Stiletto", "Stiletto", null, false));
-            Modlist.Add(new Toggleables("VRMod", "VRMod", "PlayHomeVR", delegate(bool b)
+            Modlist.Add(new Toggleables("HoneyPot", Localizable.ToggleHoneyPot, "HoneyPot", null, true));
+            Modlist.Add(new Toggleables("RimRemover", Localizable.ToggleRimRemover, "RimRemover", null, false));
+            Modlist.Add(new Toggleables("Stiletto", Localizable.ToggleStiletto, "Stiletto", null, false));
+            Modlist.Add(new Toggleables("VRMod", Localizable.ToggleVRMod, "PlayHomeVR", delegate(bool b)
             {
                 if (b)
                     MessageBox.Show("To use this mod, open SteamVR before opening either the main game or studio.", "Usage");
