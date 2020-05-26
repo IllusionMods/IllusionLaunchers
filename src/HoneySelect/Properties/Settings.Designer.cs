fe5d7d19c5056ae01ea34ff33198ec6ce75691f9
ScrewThisNoise <51236954+ScrewThisNoise@users.noreply.github.com>
2020-05-26 02:51:05 +0200

    Started updating HS Launcher

--- src/HoneySelect/Properties/Settings.Designer.cs
+++ src/HoneySelect/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace InitDialog.Properties {
+namespace InitSetting.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
