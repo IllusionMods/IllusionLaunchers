b92af19756d78dda9d3af8d2206d3e7246b7f09c
ScrewThisNoise <51236954+ScrewThisNoise@users.noreply.github.com>
2021-02-09 18:40:25 +0100

    Upped version

--- src/SharedCode/AssemblyInfo.cs
+++ src/SharedCode/AssemblyInfo.cs
@@ -19,7 +19,7 @@ using System.Windows;
 [assembly: AssemblyCopyright("GPL-3.0")]
 [assembly: AssemblyTrademark("")]
 
-[assembly: AssemblyVersion("3.0.0"
+[assembly: AssemblyVersion("3.0.1"
 #if DEBUG
     + ".*")]
 #else
