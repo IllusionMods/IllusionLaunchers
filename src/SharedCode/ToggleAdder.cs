19716cfc81caf400a0a0ae5c3a4a7e06c03e77f1
ScrewThisNoise <51236954+ScrewThisNoise@users.noreply.github.com>
2021-04-13 22:32:58 +0200

    fixed settings disappearing bug

--- src/SharedCode/ToggleAdder.cs
+++ src/SharedCode/ToggleAdder.cs
@@ -121,6 +121,7 @@ namespace InitSetting
                     }
                 }, true),
                 new PluginToggle("RimRemover", Localizable.ToggleRimRemover, "", "RimRemover", null, false),
+                new PluginToggle("AutoSave", "Activate Autosave", "", "*AutoSave", null, false),
                 new PluginToggle("ShortcutPlugin", Localizable.ToggleShortcutHS, "", "ShortcutHSParty", null, true),
                 new PluginToggle("Stiletto", Localizable.ToggleStiletto, Localizable.TooltipGGmod, "Stiletto", null, false),
                 new PluginToggle("VRMod", Localizable.ToggleVRMod, Localizable.TooltipVRMod, "PlayHomeVR", delegate (bool b)
