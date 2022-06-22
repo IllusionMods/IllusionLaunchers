12c2ab9737a3007793602ccad48aa3821f70bc03
ScrewThisNoise <foreveryounghentai@gmail.com>
2022-06-22 13:51:31 +0200

    Added HS2 Steam Launcher

---
+++ src/HoneySelect 2 Steam/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Resources;
+using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Windows;
+
+[assembly: CompilationRelaxations(8)]
+[assembly: RuntimeCompatibility(WrapNonExceptionThrows = true)]
+[assembly: Debuggable(DebuggableAttribute.DebuggingModes.IgnoreSymbolStoreSequencePoints)]
+[assembly: AssemblyTitle("PlayHome Launcher")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyProduct("")]
+[assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
+[assembly: NeutralResourcesLanguage("en-GB")]
+
