08a85a48d4d10cb835dad316c694d33b6aa11251
Splendide Imaginarius <119545140+Splendide-Imaginarius@users.noreply.github.com>
2024-09-19 20:25:47 +0000

    Fix 3840x2400 typo (#26)

--- src/SharedSettingsNew/SettingManagerNew.cs
+++ src/SharedSettingsNew/SettingManagerNew.cs
@@ -142,7 +142,7 @@ namespace InitSetting
             {
                 Width = 3840,
                 Height = 2400,
-                text = "3840 x 2160 (16 : 10)"
+                text = "3840 x 2400 (16 : 10)"
             }
         };
 
